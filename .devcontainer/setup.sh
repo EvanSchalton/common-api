c0f8702ed809b6b622213214390fc8e16967c1d2
Evan Schalton <Evan.Schalton@Gmail.com>
2021-12-03 12:49:10 +0000

    chore: setup repo
    added commit mgmt, docker devcontainer and python deps

---
+++ .devcontainer/setup.sh
@@ -0,0 +1,21 @@
+sudo apt-get install libxml2-dev libxslt1-dev zlib1g-dev libffi-dev libssl-dev
+pip3 install --user -r requirements.txt
+
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
+
+
+npm install commitizen -g
+commitizen init cz-conventional-changelog --save-dev --save-exact
+
+
+npm install --save-dev @commitlint/cli
+npm install --save-dev @commitlint/config-conventional
+echo "module.exports = {extends: ['@commitlint/config-conventional']}" > commitlint.config.js
+
+
+npm install husky --save-dev
+npx husky install
+npx husky add .husky/commit-msg 'npx --no -- commitlint --edit "$1"'
+npx husky add .husky/prepare-commit-msg "exec < /dev/tty && git cz --hook || true"
